449c40c86ad4b90e428bafa0963de46672aeda8e
megumi <149559315+megumihoshino@users.noreply.github.com>
2024-05-29 18:02:39 +0700

    Add files via upload
    msh belajar tapi tiba2 mager bjirr :")

---
+++ Main.java
@@ -0,0 +1,13 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main{
+  public static void main(String[] args){
+    System.out.println("goodbye world");
+  }
+}
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
